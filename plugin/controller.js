14dcb011b74dc5106c882aff1158f54574b28843
Nicolas Siver <nicolas@siver.im>
2015-11-21 22:18:23 -0500

    spoiler replacement

--- plugin/controller.js
+++ plugin/controller.js
@@ -1,9 +1,23 @@
 (function (Controller) {
     'use strict';
 
-    var spoiler = /:{3,}([\s\S]+?):{3,}/g;
+    var spoiler  = /:{3,}([\s\S]+?):{3,}/g,
+        template = '<div class="ns-spoiler"><div class="ns-spoiler-control"><a class="btn btn-default" href="#">Spoiler</a></div><div class="ns-spoiler-content">$1</div></div>';
 
+    /**
+     * Performs replacements on content field.
+     *
+     * @param payload {object} - includes full post entity Payload.postData.content
+     * @param callback returns updated content
+     */
     Controller.parsePost = function (payload, callback) {
+        var content = payload.postData.content;
+
+        if (content) {
+            content = content.replace(spoiler, template);
+            payload.postData.content = content;
+        }
+
         callback(null, payload);
     };
 
