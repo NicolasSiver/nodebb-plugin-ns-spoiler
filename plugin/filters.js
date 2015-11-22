9b2867b15e2fee78d351cccc35ce7d3217543db7
Nicolas Siver <nicolas@siver.im>
2015-11-22 07:58:32 -0500

    spoiler hint

--- plugin/filters.js
+++ plugin/filters.js
@@ -3,6 +3,13 @@
 
     var controller = require('./controller');
 
+    Filters.composerHelp = function (helpText, callback) {
+        helpText += '<h2>Spoiler</h2>';
+        helpText += 'To hide content you could use spoilers:';
+        helpText += '\n<pre>\n:::\nspoiler content, will be hidden under interactive button\n:::</pre>';
+        callback(null, helpText);
+    };
+
     Filters.parsePost = function (payload, callback) {
         controller.parsePost(payload, callback);
     };
