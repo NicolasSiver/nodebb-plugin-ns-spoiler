67990bee9f5fed376c78be8255d329be631b43fc
Nicolas Siver <nicolas@siver.im>
2015-11-23 21:38:02 -0500

    safe checks for lists and initial paragraph

--- plugin/controller.js
+++ plugin/controller.js
@@ -1,9 +1,11 @@
 (function (Controller) {
     'use strict';
 
-    var spoiler      = /:{3,}\s*(?:<br\s*\/?>\s*)*([\s\S]+?):{3,}/g,
-        sanitizeWrap = /<(\w+)[^<]*>(:{3,})<\/\1>/g,
-        template     = '<div class="ns-spoiler" data-open="false"><div class="ns-spoiler-control"><a class="btn btn-default" href="#"><i class="fa fa-eye"></i> spoiler</a></div><div class="ns-spoiler-content">$1</div></div>';
+    var spoiler          = /:{3,}\s*(?:<br\s*\/?>\s*)*([\s\S]+?):{3,}/g,
+        sanitizeWrap     = /<(\w+)[^<]*>(:{3,})<\/\1>/g,
+        safeCloseForList = /(<(ul|ol)>[\s\S]+?)(:{3,})([\s\S]+?<\/\2>)/g,
+        safeInitialText  = /<p>(:{3,})<br[^<]*>\n([\s\S]*)<\/p>/g,
+        template         = '<div class="ns-spoiler" data-open="false"><div class="ns-spoiler-control"><a class="btn btn-default" href="#"><i class="fa fa-eye"></i> spoiler</a></div><div class="ns-spoiler-content">$1</div></div>';
 
     /**
      * Performs replacements on content field.
@@ -15,8 +17,13 @@
         var content = payload.postData.content;
 
         if (content) {
-            // Sanitize: remove wrapping tags, like <p>
-            content = content.replace(sanitizeWrap, '$2');
+            // 1. Sanitize: remove wrapping tags, like <p>
+            // 2. Fix not properly closed <ul> and <ol> lists
+            // 3. Fix initial text lines, that concat with spoiler via paragraph
+            content = content
+                .replace(sanitizeWrap, '$2')
+                .replace(safeCloseForList, '$1$4\n$3')
+                .replace(safeInitialText, '$1\n<p>$2</p>');
             console.log(content);
             content = content.replace(spoiler, template);
             payload.postData.content = content;
