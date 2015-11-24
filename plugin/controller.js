97a0fd1286e9d20e188cbf5084faedd45819b57e
Nicolas Siver <nicolas@siver.im>
2015-11-23 22:05:29 -0500

    paragraph shifts

--- plugin/controller.js
+++ plugin/controller.js
@@ -4,7 +4,8 @@
     var spoiler          = /:{3,}\s*(?:<br\s*\/?>\s*)*([\s\S]+?):{3,}/g,
         sanitizeWrap     = /<(\w+)[^<]*>(:{3,})<\/\1>/g,
         safeCloseForList = /(<(ul|ol)>[\s\S]+?)(:{3,})([\s\S]+?<\/\2>)/g,
-        safeInitialText  = /<p>(:{3,})<br[^<]*>\n([\s\S]*)<\/p>/g,
+        safeShiftStart   = /^(<p>)(:{3,})$/gm,
+        safeShiftEnd     = /^(:{3,})(<\/p>)$/gm,
         template         = '<div class="ns-spoiler" data-open="false"><div class="ns-spoiler-control"><a class="btn btn-default" href="#"><i class="fa fa-eye"></i> spoiler</a></div><div class="ns-spoiler-content">$1</div></div>';
 
     /**
@@ -16,14 +17,15 @@
     Controller.parsePost = function (payload, callback) {
         var content = payload.postData.content;
 
+        // 1. Sanitize: remove wrapping tags, like <p>
+        // 2. Fix not properly closed <ul> and <ol> lists
+        // 3. Fix text lines at Start, that concatenates with spoiler via paragraph
         if (content) {
-            // 1. Sanitize: remove wrapping tags, like <p>
-            // 2. Fix not properly closed <ul> and <ol> lists
-            // 3. Fix initial text lines, that concat with spoiler via paragraph
             content = content
                 .replace(sanitizeWrap, '$2')
                 .replace(safeCloseForList, '$1$4\n$3')
-                .replace(safeInitialText, '$1\n<p>$2</p>');
+                .replace(safeShiftStart, '$2$1')
+                .replace(safeShiftEnd, '$2$1');
             console.log(content);
             content = content.replace(spoiler, template);
             payload.postData.content = content;
