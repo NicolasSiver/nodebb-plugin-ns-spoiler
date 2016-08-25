fda95e632f3a4ca3db52913fa203e739bc86cea5
Barış Soner Uşaklı <barisusakli@gmail.com>
2016-08-26 00:26:59 +0300

    dont require composer on page load
    load it when it's opened

--- public/js/composer.js
+++ public/js/composer.js
@@ -3,29 +3,35 @@
 $(document).ready(function () {
     'use strict';
 
-    require([
-        'composer/formatting', 'composer/controls'
-    ], function (formatting, controls) {
+    $(window).on('action:composer.enhanced', function() {
+		prepareFormattingTools();
+	});
 
-        var tag    = ':::',
-            nl     = '\n',
-            prompt = 'spoiler text';
-
-        formatting.addButtonDispatch('ns-spoiler', composerControlDidClick);
-
-        function composerControlDidClick(textArea, selectionStart, selectionEnd) {
-            if (selectionStart === selectionEnd) {
-                var hlContentStart = selectionStart + tag.length + nl.length,
-                    hlContentEnd   = hlContentStart + prompt.length;
-                controls.insertIntoTextarea(textArea, getNewSpoiler());
-                controls.updateTextareaSelection(textArea, hlContentStart, hlContentEnd);
-            } else {
-                controls.wrapSelectionInTextareaWith(textArea, tag, tag);
+    function prepareFormattingTools() {
+        require([
+            'composer/formatting', 'composer/controls'
+        ], function (formatting, controls) {
+    
+            var tag    = ':::',
+                nl     = '\n',
+                prompt = 'spoiler text';
+    
+            formatting.addButtonDispatch('ns-spoiler', composerControlDidClick);
+    
+            function composerControlDidClick(textArea, selectionStart, selectionEnd) {
+                if (selectionStart === selectionEnd) {
+                    var hlContentStart = selectionStart + tag.length + nl.length,
+                        hlContentEnd   = hlContentStart + prompt.length;
+                    controls.insertIntoTextarea(textArea, getNewSpoiler());
+                    controls.updateTextareaSelection(textArea, hlContentStart, hlContentEnd);
+                } else {
+                    controls.wrapSelectionInTextareaWith(textArea, tag, tag);
+                }
             }
-        }
-
-        function getNewSpoiler() {
-            return tag + nl + prompt + nl + tag;
-        }
-    });
+    
+            function getNewSpoiler() {
+                return tag + nl + prompt + nl + tag;
+            }
+        });
+    }
 });
